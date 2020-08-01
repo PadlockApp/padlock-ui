925ce88cee55a5ecb3e4b7ecdfe14db5e09ff21b
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-08-02 00:53:05 +0300

    Implemented profile page + profile image

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,58 @@ import { useSelector } from 'react-redux';
 import { State } from './reducers/types';
 import { Eth } from './helper';
 import { useQuery } from '@apollo/client';
+//@ts-ignore
+import IPFS from 'ipfs-api';
+import axios from 'axios';
 import './style.scss';
+
 // import { ThreadID } from '@textile/hub';
 // import { FileDocument } from './schemas';
+const { REACT_APP_PINATA_KEY, REACT_APP_PINATA_SECRET } = process.env;
+
+const ipfs = new IPFS({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+});
+
+export const pinByHash = (hashToPin: string) => {
+  const url = `https://api.pinata.cloud/pinning/pinByHash`;
+  const body = {
+    hashToPin: hashToPin,
+  };
+  return axios
+    .post(url, body, {
+      headers: {
+        pinata_api_key: REACT_APP_PINATA_KEY,
+        pinata_secret_api_key: REACT_APP_PINATA_SECRET,
+      },
+    })
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      return error;
+    });
+};
 
 function App() {
+  const space = useSelector((state: State) => state.space);
+
   const [navExpanded, setNavExpanded] = useState<boolean>(false);
   const toggleNavExpanded = () => setNavExpanded(!navExpanded);
+  const [imgUrl, setImgUrl] = useState('https://i.imgur.com/4b2rBVh.png');
+
+  useEffect(() => {
+    (async () => {
+      if (space) {
+        const profileImgHash = await space.public.get('profile-img-hash');
+        if (profileImgHash) {
+          setImgUrl(`https://gateway.pinata.cloud/ipfs/${profileImgHash}`);
+        }
+      }
+    })();
+  }, [space]);
 
   return (
     <Router>
@@ -30,10 +75,7 @@ function App() {
                   <div className="profile media">
                     <div className="media-left">
                       <figure className="image is-128x128">
-                        <img
-                          className="is-rounded"
-                          src="https://i.imgur.com/4b2rBVh.png"
-                        />
+                        <img className="is-rounded" src={imgUrl} />
                       </figure>
                     </div>
                   </div>
@@ -55,7 +97,7 @@ function App() {
                     </NavLink>
 
                     <NavLink
-                      to="/discover"
+                      to="/browse"
                       activeClassName="is-active"
                       className="subtitle is-7"
                     >
@@ -65,7 +107,22 @@ function App() {
                 </ul>
               </aside>
             </div>
-            <div className="column">hi</div>
+            <div className="column">
+              <Switch>
+                <Route path="/account">
+                  <Account />
+                </Route>
+                <Route path="/publish">
+                  <Publish />
+                </Route>
+                <Route path="/browse">
+                  <Browse />
+                </Route>
+                <Route path="/">
+                  <Redirect to="/create" />
+                </Route>
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
@@ -131,7 +188,7 @@ function Publish() {
           <div className="column is-half">
             <div className="field">
               <label className="label">Title</label>
-              <div className="control has-icons-right">
+              <div className={`control has-icons-right`}>
                 <input
                   className={`input ${titleIsInvalid ? 'is-danger' : ''}`}
                   type="text"
@@ -269,15 +326,25 @@ function Publish() {
 
 function Account() {
   const space = useSelector((state: State) => state.space);
-  const secretPair = useSelector((state: State) => state.secretPair);
 
   const [name, setName] = useState('Loading..');
-  const [secretAddress, setSecretAddress] = useState('Loading..');
+  const [website, setWebsite] = useState('Loading..');
+  const [about, setAbout] = useState('Loading..');
+  const [file, setFile] = useState<File | null>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    space?.public
-      .get('name')
-      .then((res: any) => setName(res ? res : 'anonymous'));
+    (async () => {
+      if (space) {
+        const loadedName = await space.public.get('name');
+        const loadedWebsite = await space.public.get('website');
+        const loadedAbout = await space.public.get('about');
+        setName(loadedName ? loadedName : 'anonymous');
+        setWebsite(loadedWebsite ? loadedWebsite : '');
+        setAbout(loadedAbout ? loadedAbout : '');
+        setLoading(false);
+      }
+    })();
   }, [space]);
   const nameIsInvalid = false;
 
@@ -285,18 +352,42 @@ function Account() {
     setName(e.target.value);
   };
 
-  const updateAccount = () => {
+  const handleWebsiteChange = (e: any) => {
+    setWebsite(e.target.value);
+  };
+
+  const handleAboutChange = (e: any) => {
+    setAbout(e.target.value);
+  };
+
+  const updateAccount = async () => {
     space?.public.set('name', name);
+    space?.public.set('website', website);
+    space?.public.set('about', about);
+    const { hash } = (
+      await ipfs.files.add(Buffer.from((await file?.arrayBuffer()) as any))
+    )[0];
+    await space?.public.set('profile-img-hash', hash);
+    const a = await pinByHash(hash);
+    console.log(a);
   };
 
   return (
     <div>
       <section className="section">
+        <h1
+          className="title"
+          style={{ marginBottom: '100px', marginTop: '20px' }}
+        >
+          Your Profile
+        </h1>
         <div className="columns is-centered">
-          <div className="column is-half">
+          <div className="column is-three-fifths">
             <div className="field">
-              <label className="label">Your account name</label>
-              <div className="control has-icons-right">
+              <label className="label">Name</label>
+              <div
+                className={`control ${loading && 'is-loading'} has-icons-right`}
+              >
                 <input
                   className={`input ${nameIsInvalid ? 'is-danger' : ''}`}
                   type="text"
@@ -315,26 +406,78 @@ function Account() {
               )}
             </div>
             <div className="field">
-              <label className="label">Your secret address</label>
-              <div className="control has-icons-right">
+              <label className="label">Website (optional)</label>
+              <div
+                className={`control ${loading && 'is-loading'} has-icons-right`}
+              >
                 <input
-                  className={'input'}
+                  className={`input ${false ? 'is-danger' : ''}`}
                   type="text"
-                  disabled={true}
-                  value={secretPair?.address || 'Loading..'}
+                  placeholder="Your website"
+                  value={website}
+                  onChange={handleWebsiteChange}
                 />
+                {false && (
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                )}
+              </div>
+              {false && (
+                <p className="help is-danger">This website is invalid</p>
+              )}
+            </div>
+            <div className="field">
+              <label className="label">
+                About yourself (max 250 characters)
+              </label>
+              <div
+                className={`control ${loading && 'is-loading'} has-icons-right`}
+              >
+                <textarea
+                  className={`textarea has-fixed-size ${
+                    false ? 'is-danger' : ''
+                  }`}
+                  placeholder="Say something about yourself"
+                  value={about}
+                  onChange={handleAboutChange}
+                />
+                {false && (
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                )}
+              </div>
+              {false && <p className="help is-danger">About text is invalid</p>}
+            </div>
+          </div>
+          <div className="column">
+            <div className="field" style={{ marginTop: '32px' }}>
+              <div className="file is-medium is-boxed is-centered has-name">
+                <label className="file-label">
+                  <input
+                    className="file-input"
+                    type="file"
+                    onChange={(e) => setFile(e.target.files?.item(0))}
+                  />
+                  <span className="file-cta">
+                    <span className="file-icon">
+                      <i className="fas fa-upload"></i>
+                    </span>
+                    <span className="file-label">Set profile image</span>
+                  </span>
+                  <span className="file-name">{file?.name && file?.name}</span>
+                </label>
               </div>
             </div>
-
-            <div className="field is-grouped">
-              <div className="control">
-                <button
-                  className="button is-link is-success"
-                  onClick={updateAccount}
-                >
-                  Save changes
-                </button>
-              </div>
+            <div className="buttons is-centered">
+              <button
+                className="button is-medium is-warning is-rounded"
+                onClick={updateAccount}
+                style={{ marginTop: '82px' }}
+              >
+                Save
+              </button>
             </div>
           </div>
         </div>
@@ -343,7 +486,7 @@ function Account() {
   );
 }
 
-function Discover() {
+function Browse() {
   // const apolloClient = useSelector((state: State) => state.apolloClient);
   // const [creations, setCreations] = useState<any[]>([]);
 
